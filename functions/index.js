be7395aa3f5f0b90aa1480192a70effd5c5ac6a3
DasDune <duneroy@gmail.com>
2020-09-22 17:36:57 +0700

    update service account file

--- functions/index.js
+++ functions/index.js
@@ -121,3 +121,27 @@ exports.popCols = functions.https.onRequest((req, res) => {
   popCol(req.body);
   res.send(cols);
 });
+
+//auth create user (testing...)
+exports.popUser = functions.https.onRequest((req, res) => {
+  admin
+    .auth()
+    .createUser({
+      email: 'user@example.com',
+      emailVerified: false,
+      phoneNumber: '+11234567890',
+      password: 'secretPassword',
+      displayName: 'John Doe',
+      photoURL: 'http://www.example.com/12345678/photo.png',
+      disabled: false,
+    })
+    .then(function (userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log('Successfully created new user:', userRecord.uid);
+      res.send('Successfully created new user:', userRecord.uid);
+    })
+    .catch(function (error) {
+      console.log('Error creating new user:', error);
+      res.send('Error creating new user:', error);
+    });
+});
