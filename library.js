df2a07bb6df69f9944231b71b140c7c168878209
dasdunetech <dasdunetech@gmail.com>
2021-10-04 14:18:22 +0700

    updates

---
+++ library.js
@@ -0,0 +1,26 @@
+// ***Library server  ***
+const express = require('express');
+const app = express();
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use('/pub', express.static('pub'));
+
+const port = 8080;
+
+const cors = require('cors');
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+
+app.listen(port, () => {
+    console.log(`TagLinker listening at port ${port}`);
+});
+
+
+// Home page
+app.get('/', (req, res) => {
+    res.send('Library ready');
+    // res.sendFile(`${__dirname}/pub/signIn.html`);
+});
+
